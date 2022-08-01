f316eef6ca87a4abcd5c11c3999da4312b4785af
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-01 19:38:17 +0200

    edit Month Table

--- database/seeders/SectionItemSeeder.php
+++ database/seeders/SectionItemSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class MonthItemSeeder extends Seeder
+class SectionItemSeeder extends Seeder
 {
     /**
      * Run the database seeds.
