6dbbc45f5e765afeda3245ec045a06fc78db212e
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-03 17:09:54 +0200

    Create Month Resource And Collection

--- app/Models/UserSubject.php
+++ app/Models/UserSubject.php
@@ -5,13 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Exam extends Model
+class UserSubject extends Model
 {
     use HasFactory;
-
-
-    public function questions()
-    {
-        return $this->hasMany(Question::class);
-    }
 }
