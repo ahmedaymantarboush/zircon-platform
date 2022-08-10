3813afd22d33133167ef0cc18e4d7086cc22b2b6
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-10 19:35:42 +0200

    Create Sections API

--- app/Http/Controllers/API/SectionController.php
+++ app/Http/Controllers/API/SectionController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SectionCollection;
+use App\Http\Resources\SectionResource;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class SectionController extends Controller
@@ -25,9 +28,32 @@ class SectionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = json_decode($request->data, true);
+        $lecture = \App\Models\Lecture::where('slug', $data['lec'])->first();
+        if (!$lecture) :
+            return apiResponse(false, _('هذخ المحاضرة غير موجودة'), [], 404);
+        endif;
+        $lecture_id = $lecture->id;
+        $secttion = $lecture->sections()->create([
+            'order' => count(Section::where('lecture_id', $lecture_id)->get()) + 1, //$request->input('order'),
+            'title' => $data['sectionTitle'],
+        ]);
+
+        return apiResponse(true, _('تم اضافة القسم بنجاح'), SectionResource::only($secttion, ['title', 'order', 'items']));
     }
 
+    public function sortSections()
+    {
+        $data = json_decode(request()->data, true);
+        foreach ($data['sections'] as $key => $val) {
+            $section = Section::find($val);
+            if (!$section) :
+                return apiResponse(false, _('هذا السكشن غير موجود'), [], 404);
+            endif;
+            $section->update(['order' => $key]);
+        }
+        return apiResponse(true, _('تم تحديث الترتيب بنجاح'),SectionCollection::only($section->lecture->sections, ['title', 'order', 'items']));
+    }
     /**
      * Display the specified resource.
      *
@@ -48,7 +74,15 @@ class SectionController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+        $section = Section::find($id);
+        if (!$section) :
+            return apiResponse(false, _('هذا السكشن غير موجود'), [], 404);
+        endif;
+        $section->update([
+            'title' => array_key_exists('new_sectionTitle',$data) ? $data['new_sectionTitle'] : $section->title,
+        ]);
+        return apiResponse(true, _('تم تحديث القسم بنجاح'), SectionResource::only($section, ['title', 'order', 'items']));
     }
 
     /**
