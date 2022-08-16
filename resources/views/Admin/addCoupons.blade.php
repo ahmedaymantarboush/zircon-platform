069fee50594c809cde0dad8ef26d7c54d6eea5bc
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-08-17 01:49:54 +0200

    Add Coupon 'Add'

---
+++ resources/views/Admin/addCoupons.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.adminLayout')
+{{-- ////////////////////////////////////////////////////////// --}}
+{{-- ///////////////////////// Css /////////////////////////// --}}
+{{-- //////////////////////////////////////////////////////// --}}
+@section('css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/add_exam.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/add_coupons.css') }}"/>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+@endsection
+{{-- ////////////////////////////////////////////////////////////////////// --}}
+{{-- ///////////////////////// Main ////////////////////////////////////// --}}
+{{-- //////////////////////////////////////////////////////////////////// --}}
+@section('content')
+    <div class="parent" style="width: 100%;">
+        <div class="bg_addExam"></div>
+        <div class="d-flex justify-content-center main_div">
+            <div style="width: 90%;">
+                <div class="main_exam_info">
+                    <form action="" class="exam-form">
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="main_title">تحرير كروت شحن جديد </p>
+                                <div class="blue_line"></div>
+                            </div>
+                            <div class="col-lg-4 col-md-12">
+                                <label for="formGroupExampleInput" class="form-label input_label">عدد الكروت :</label>
+                                <input type="number" class="form-control-lg form-control" id="formGroupExampleInput"
+                                    placeholder="ادخل عدد الكروت" style="font-size: 15px;width: 90%;">
+                            </div>
+                            <div class="col-lg-4 col-md-12">
+                                <label for="formGroupExampleInput" class="form-label input_label">اختر المكان :</label>
+                                <select
+                                    class="form-select form-select-lg search-select-box select_part"
+                                    name="place"
+                                    id="formGroupExampleInput" data-live-search="true">
+                                    <option selected>
+                                        اختر المكان
+                                    </option>
+                                    <option value="الصف الأول الثانوي">
+                                        الصف الأول الثانوي
+                                    </option>
+                                    <option value="الصف الثاني الثانوي">
+                                        الصف الثاني الثانوي
+                                    </option>
+                                    <option value="الصف الثالث الثانوي">
+                                        الصف الثالث الثانوي
+                                    </option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-12">
+                                <label for="formGroupExampleInput" class="form-label input_label">قيمة الكارت :</label>
+                                <input type="number" class="form-control-lg form-control" id="formGroupExampleInput"
+                                       placeholder="ادخل القيمة للكارت الواحد" style="font-size: 15px;width: 90%;">
+                            </div>
+
+                            <div class="col-12">
+                                <div class="end-line"></div>
+                                <button type="submit" class="sub_btn">طباعة</button>
+                                <a href="#" class="a_btn">سجل كروت الشحن</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="main_exam_info">
+                        <div class="row" style="padding-bottom: 30px">
+                            <div class="col-12 d-flex justify-content-between">
+                                <section>
+                                    <p class="main_title">تحرير كروت شحن جديد </p>
+                                    <div class="blue_line"></div>
+                                </section>
+                                <button type="button" class="sub_btn print_btn" style="margin-left: 50px;">اضافة</button>
+                            </div>
+                            <div class="col-lg-4 col-sm-12" style="padding-top: 30px">
+                                @include('components.Admin.coupon-card',[
+                                    'counter'=>'1',
+                                    'id'=>'5',
+                                    'start_date'=>'27-7-2022:17:50',
+                                    'end_date'=>'27-7-2023:17:50',
+                                    'value'=>'50',
+                                    'code'=>'as56d4ef55',
+                                    'app_name'=>'أ/ محمد البري',
+                                ])
+
+                            </div>
+
+                        </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+
+
+
+@section('javascript')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+    </script>
+
+    <!-- text editor  -->
+    <!-- موجود هنا والايديت  -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+
+    {{-- <!-- main js file --> --}}
+    {{-- <!-- موجود في كل الصفحات  --> --}}
+
+    {{-- Duration picker --}}
+    {{-- الصفحة دي بس --}}
+    <script src="https://cdn.jsdelivr.net/npm/html-duration-picker@latest/dist/html-duration-picker.min.js"></script>
+    <style>
+        .scroll-btn {
+            visibility: hidden;
+        }
+    </style>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+    <!-- bootstrap 5  -->
+    <!-- موجود في كل الصفحات  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('admin/assets/js/main.js') }}"></script>
+    <script src="{{ asset('js/qrcode.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/add-coupons.js') }}"></script>
+
+@endsection
+
---
+++ resources/views/components/Admin/coupon-card.blade.php
@@ -0,0 +1,151 @@
+<div class="coupon-card">
+    <div class="title-div ">
+        <span class="date" style="margin-left: 25px">{{$start_date}}</span>
+        <span class="tname" style="margin-left: 25px">{{$app_name}}</span>
+        <img src="{{asset('admin/assets/imgs/logo-white.png')}}"style="width: 50px;">
+    </div>
+    <div class="card_body">
+        <span class="body_title">كرت شحن رصيد</span>
+        <div class="black_line"></div>
+    </div>
+    <div class="row card-body2">
+        <div class="col-8" style="margin-top: 20px">
+            <p>
+                <span class="date-title">تاريخ التحرير :</span>
+                <span class="date-text">{{$start_date}}</span>
+            </p>
+            <p style="margin-top: -10px">
+                <span class="date-title">تاريخ الانتهاء :</span>
+                <span class="date-text">{{$end_date}}</span>
+            </p>
+            <p style="margin-top: -10px">
+                <span style="font-size: 15px;font-weight: 600;color: black;">القيمة :</span>
+                <span dir="rtl">
+                    <span class="date-title" style="font-weight: 700;">{{$value}}</span>
+                    <span style="color: black;font-size: 10px">ج.م</span>
+                </span>
+            </p>
+        </div>
+        <div class="col-4">
+            <div id="qr_tag_{{$counter}}" class="qr-tag"></div>
+        </div>
+    </div>
+    <div class="card-code">
+        <span>{{$code}}</span>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="374" height="244" viewBox="0 0 374 244">
+        <g id="Group_286" data-name="Group 286" transform="translate(-191 -517)">
+            <g id="Rectangle_84" data-name="Rectangle 84" transform="translate(207 517)" fill="#fff" stroke="#000" stroke-width="1" stroke-dasharray="10">
+                <rect width="341" height="241" stroke="none"/>
+                <rect x="0.5" y="0.5" width="340" height="240" fill="none"/>
+            </g>
+            <path id="Path_20" data-name="Path 20" d="M0,0H341V64H0Z" transform="translate(207 517)"/>
+            <g id="Group_230" data-name="Group 230" transform="translate(58 -109)">
+                <g id="Ellipse_8" data-name="Ellipse 8" transform="translate(136 817)" fill="#fff" stroke="#000" stroke-width="1" stroke-dasharray="5">
+                    <circle cx="13.5" cy="13.5" r="13.5" stroke="none"/>
+                    <circle cx="13.5" cy="13.5" r="13" fill="none"/>
+                </g>
+                <rect id="Rectangle_85" data-name="Rectangle 85" width="16" height="67" transform="translate(133 803)" fill="#fff"/>
+            </g>
+            <g id="Group_229" data-name="Group 229" transform="translate(59 -109)">
+                <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(478 817)" fill="#fff" stroke="#000" stroke-width="1" stroke-dasharray="5">
+                    <circle cx="13.5" cy="13.5" r="13.5" stroke="none"/>
+                    <circle cx="13.5" cy="13.5" r="13" fill="none"/>
+                </g>
+                <rect id="Rectangle_86" data-name="Rectangle 86" width="16" height="67" transform="translate(490 803)" fill="#fff"/>
+            </g>
+            <line id="Line_18" data-name="Line 18" x2="303" transform="translate(226.5 719.5)" fill="none" stroke="#c2c2c2" stroke-width="1" stroke-dasharray="10"/>
+        </g>
+    </svg>
+
+
+</div>
+<style>
+    .coupon-card{
+        position: absolute;
+        width: 100%;
+    }
+    .title-div{
+        position: absolute;
+        z-index: 501;
+        margin-right: 45px;
+        margin-top: 10px;
+    }
+    .card_body{
+        position: absolute;
+        z-index: 502;
+        margin-right: 45px;
+        margin-top: 80px;
+    }
+    .svg_img{
+        z-index: 500;
+    }
+    .date{
+        font-size: 12px;
+        font-weight: 600;
+        color: #CACACB;
+    }
+    .tname{
+        font-size: 15px;
+        font-weight: 700;
+        color: #ffffff;
+    }
+    .body_title{
+        color: black;
+        font-weight: 600;
+        font-size: 15px;
+    }
+    .black_line{
+        background-color: black;
+        width: 40px;
+        height: 2px;
+        margin-top: 5px;
+        margin-bottom: 10px;
+    }
+    .qr-tag{
+        margin-top: -5px;
+
+    }
+    .card-body2{
+        position: absolute;
+        width: 300px;
+        margin-top: 100px;
+        margin-right: 40px;
+    }
+    .date-title{
+        font-size: 12px;
+        color: black;
+    }
+    .date-text{
+        color: #8D8D8D;
+        font-size: 12px;
+    }
+    .card-code{
+        position: absolute;
+        margin-right: 100px;
+        margin-top: 211px;
+    }
+    .card-code span{
+        color: black;
+        font-weight: 500;
+        font-size: 13px;
+        letter-spacing: 10px;
+    }
+
+
+</style>
+<script>
+    ///////////////////////////////////////////////////
+    /////////////////////QR Code//////////////////////
+    /////////////////////////////////////////////////
+    $(document).ready(function (){
+        let qr_tag_{{$counter}} = document.querySelector('#qr_tag_{{$counter}}');
+        let qr = new QRCode(qr_tag_{{$counter}},{
+            text: '{{$value}}'+'//'+'{{$id}}',
+            width :90,
+            height: 90,
+        });
+        qrcode.makeCode(qr);
+    });
+</script>
+
