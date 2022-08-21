fd898114b96401130e913c64a5c144521b57b92a
ali alaa <alialaa932003@gmail.com>
2022-08-21 14:11:20 +0200

    add examStudent files

---
+++ new/assets/js/examsStudents.js
@@ -0,0 +1,32 @@
+let tableParent = document.querySelector(".lectures-table");
+let trTable = document.querySelectorAll(".lectures-table tbody tr");
+let openBtn = document.querySelectorAll(".open-tr");
+/// run button
+openBtn.forEach((ele) => {
+	ele.addEventListener("click", function () {
+		trTable.forEach((e) => {
+			let myElement = ele.parentElement.parentElement;
+			if (e == ele.parentElement.parentElement) {
+				myElement.classList.toggle("active-tr");
+
+				myElement
+					.querySelector(".open-tr svg")
+					.classList.toggle("fa-minus");
+				myElement
+					.querySelector(".open-tr svg")
+					.classList.toggle("fa-plus");
+				return;
+			}
+
+			e.querySelector(".open-tr svg").classList.remove("fa-minus");
+			e.querySelector(".open-tr svg").classList.add("fa-plus");
+
+			e.classList.remove("active-tr");
+		});
+	});
+});
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+});
+let barcodeText = document.querySelector(".barcodeText");
+JsBarcode("#profileBarCode", barcodeText.textContent);
