80eebdc60ff22e119ecd751400447ca4b470f721
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-21 12:53:00 +0200

    Solve Design Problem

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -1,13 +1,11 @@
+<!--jquery-->
+<!-- موجود في كل الصفحات  -->
 
-		<!--jquery-->
-		<!-- موجود في كل الصفحات  -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+@yield('javascript')
+<script src="{{ asset('admin/assets/js/main.js') }}"></script>
 
-		<script
-			src="https://code.jquery.com/jquery-3.6.0.min.js"
-			integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-			crossorigin="anonymous"
-		></script>
-        @yield('javascript')
-    </body>
+</body>
+
 </html>
-
