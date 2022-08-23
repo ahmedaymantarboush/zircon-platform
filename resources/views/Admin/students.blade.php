0cc442c3972b286edea5fedab568178b1f0bfd75
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-23 22:29:04 +0200

    Remove gitgub problem text

--- resources/views/Admin/students.blade.php
+++ resources/views/Admin/students.blade.php
@@ -729,9 +729,6 @@
 @section('javascript')
 <script src="{{ asset('admin/assets/js/jsBarCode.all') }}.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/jsbarcode@3.8.0/dist/JsBarcode.all.min.js"></script> -->
-<<<<<<< HEAD
-
-
 <!--font awesome-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
     integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
