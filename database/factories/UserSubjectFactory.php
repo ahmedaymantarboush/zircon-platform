0be168a80451b783968adb295936a780644ac131
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-25 01:53:16 +0200

    solve parts problem

---
+++ database/factories/UserSubjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Subject;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserSubject>
+ */
+class UserSubjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => fake()->randomElement(User::all()->pluck('id')),
+            'subject_id' => fake()->randomElement(Subject::all()->pluck('id'))
+        ];
+    }
+}
