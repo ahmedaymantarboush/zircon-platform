d675632ed944cc69ff15b85b5a96b8f71acb765a
ali alaa <alialaa932003@gmail.com>
2022-08-24 19:52:54 +0200

    f

--- public/admin/assets/js/lectures.js
+++ public/admin/assets/js/lectures.js
@@ -95,7 +95,7 @@ document.querySelector("table").addEventListener("click", async function (e) {
     form.append("data", JSON.stringify(sendObj));
 
     let myResponse = await editFun(
-        `${window.location.protocol}//${window.location.host}/api/lectures/fastEdit`,
+        `${window.location.protocol}//${window.location.host}/api/lecture/fastEdit`,
         form,
         e
     );
