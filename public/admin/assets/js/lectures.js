3bcbf733e28320e5c6955cfeac390f6af7b5e0a0
ali alaa <alialaa932003@gmail.com>
2022-08-24 19:50:39 +0200

    s

--- public/admin/assets/js/lectures.js
+++ public/admin/assets/js/lectures.js
@@ -85,7 +85,7 @@ let editFun = async function (url, myData, el = null) {
 
 document.querySelector("table").addEventListener("click", async function (e) {
     if (!e.target.classList.contains("fastModiBtn")) return;
-    let dataId = e.target.closest("tr").querySelector(".number").dataset.slug;
+    let dataId = e.target.closest("tr").dataset.slug;
     console.log(dataId);
     let sendObj = {
         id: dataId,
