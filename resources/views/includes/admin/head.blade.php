c0dadedb39454d4a990a669810564cc30fe57f45
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-24 06:37:21 +0200

    Solve JQ Problem

--- resources/views/includes/admin/head.blade.php
+++ resources/views/includes/admin/head.blade.php
@@ -1,39 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link
-			href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-			rel="stylesheet"
-		/>
-		<link
-			href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600&display=swap"
-			rel="stylesheet"
-		/>
-		<!-- font awesome -->
-		<!-- موجود في كله  -->
-        <script src="https://kit.fontawesome.com/f2178052c7.js" crossorigin="anonymous"></script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600&display=swap"
+        rel="stylesheet" />
+    <!-- font awesome -->
+    <!-- موجود في كله  -->
+    <script src="https://kit.fontawesome.com/f2178052c7.js" crossorigin="anonymous"></script>
 
 
-        <!-- موجود في كله  -->
-		<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-		crossorigin="anonymous"
-		/>
+    <!-- موجود في كله  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 
-		<!-- css file -->
-		<!-- موجود في كله  -->
+    <!-- css file -->
+    <!-- موجود في كله  -->
 
-        @yield('css')
+    @yield('css')
 
-		<!--bootstrap-->
+    <!--bootstrap-->
 
-		<link rel="stylesheet" href="{{ asset('admin/assets/css/responsive.css') }}" />
-		<title>{{ config('app.name') }}</title>
-	</head>
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/responsive.css') }}" />
+    <title>{{ config('app.name') }}</title>
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+</head>
 
-	<body>
+<body>
