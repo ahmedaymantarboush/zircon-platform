50c9a6ca7954ddcdbcf21cbc858a4ed7bd65c8dc
ali alaa <alialaa932003@gmail.com>
2022-08-24 16:33:07 +0200

    g

--- public/admin/assets/js/lectures.js
+++ public/admin/assets/js/lectures.js
@@ -6,74 +6,126 @@ console.log(openBtn);
 
 /// set even and odd class
 trTable.forEach((ele, index) => {
-	if (index % 2 == 0) {
-		ele.classList.add("odd");
-	} else {
-		ele.classList.add("even");
-	}
+    if (index % 2 == 0) {
+        ele.classList.add("odd");
+    } else {
+        ele.classList.add("even");
+    }
 });
 
 /// run button
 openBtn.forEach((ele) => {
-	ele.addEventListener("click", function () {
-		trTable.forEach((e) => {
-			let myElement = ele.parentElement.parentElement;
-			if (e == ele.parentElement.parentElement) {
-				myElement.classList.toggle("active-tr");
+    ele.addEventListener("click", function () {
+        trTable.forEach((e) => {
+            let myElement = ele.parentElement.parentElement;
+            if (e == ele.parentElement.parentElement) {
+                myElement.classList.toggle("active-tr");
 
-				myElement
-					.querySelector(".open-tr svg")
-					.classList.toggle("fa-minus");
-				myElement
-					.querySelector(".open-tr svg")
-					.classList.toggle("fa-plus");
-				return;
-			}
+                myElement
+                    .querySelector(".open-tr svg")
+                    .classList.toggle("fa-minus");
+                myElement
+                    .querySelector(".open-tr svg")
+                    .classList.toggle("fa-plus");
+                return;
+            }
 
-			e.querySelector(".open-tr svg").classList.remove("fa-minus");
-			e.querySelector(".open-tr svg").classList.add("fa-plus");
+            e.querySelector(".open-tr svg").classList.remove("fa-minus");
+            e.querySelector(".open-tr svg").classList.add("fa-plus");
 
-			e.classList.remove("active-tr");
-		});
-	});
+            e.classList.remove("active-tr");
+        });
+    });
 });
 
 /**text editor */
 ClassicEditor.create(document.querySelector(".text-editor2"), {
-	language: {
-		// The UI will be English.
-		ui: "en",
+    language: {
+        // The UI will be English.
+        ui: "en",
 
-		// But the content will be edited in Arabic.
-		content: "ar",
-	},
+        // But the content will be edited in Arabic.
+        content: "ar",
+    },
 })
-	.then((editor) => {
-		const wordCountPlugin = editor.plugins.get("WordCount");
-		const wordCountWrapper = document.getElementById("word-count");
+    .then((editor) => {
+        const wordCountPlugin = editor.plugins.get("WordCount");
+        const wordCountWrapper = document.getElementById("word-count");
 
-		wordCountWrapper.appendChild(wordCountPlugin.wordCountContainer);
-		window.editor = editor;
-	})
-	.catch((err) => {});
+        wordCountWrapper.appendChild(wordCountPlugin.wordCountContainer);
+        window.editor = editor;
+    })
+    .catch((err) => {});
 
-/**** quick modify* */
-let modifyBtns = document.querySelectorAll(".q-modify");
-let addressInput = document.getElementById("address-lec");
-modifyBtns.forEach((ele) => {
-	ele.addEventListener("click", function () {
-		let text = ele
-			.closest("tr")
-			.querySelector(".name-lesson")
-			.textContent.trim();
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+// ajax
+let editFun = async function (url, myData, el = null) {
+    try {
+        let postData = await fetch(url, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "X-CSRF-TOKEN": window.csrf_token.value,
+            },
+            body: myData,
+        });
 
-		addressInput.value = text.replace(/\s/g, "-").replace(/-/g, " ");
-		// .textContent.replace(/\t/, "s");
+        let responseData = await postData.json();
 
-		// addressInput.value = lol.replace(/(\t)/gm, " ");
+        if (postData.status == 200) {
+            return responseData;
+        }
+        if (postData.status == 404) {
+            return null;
+        }
+        return null;
+    } catch (err) {}
+};
 
-		console.log(text);
-	});
+document.querySelector("table").addEventListener("click", async function (e) {
+    if (!e.target.classList.contains("editCenter")) return;
+    let dataId = e.target.closest("tr").querySelector(".number").dataset.id;
+    console.log(dataId);
+    let sendObj = {
+        id: 2,
+    };
+    let inputId = document.querySelector("#editLocationModal #trId");
+    inputId.value = 2;
+
+    form = new FormData();
+    form.append("data", JSON.stringify(sendObj));
+
+    let myResponse = await editFun(
+        `${window.location.protocol}//${window.location.host}/api/centers/fastEdit`,
+        form,
+        e
+    );
+    let objData = myResponse.data;
+    let nameOfCenter = document.querySelector(
+        ".editLocationModal .nameOfCenter"
+    );
+    let urlOfCenter = document.querySelector(".editLocationModal .urlOfCenter");
+    let newGovernorateInner = document.querySelector(
+        ".editLocationModal .newGovernorateParent .filter-option-inner-inner"
+    );
+    let newGovernorateOptions = document.querySelector(
+        ".editLocationModal .newGovernorateParent option"
+    );
+    nameOfCenter.value = null;
+    urlOfCenter.value = null;
+    let fillSelectFunction = function (options, selectInner, data) {
+        options.forEach((ele) => {
+            if (ele.value == data) {
+                ele.setAttribute("selected", "");
+                selectInner.textContent = ele.textContent;
+            } else {
+                ele.removeAttribute("selected");
+            }
+        });
+    };
+    fillSelectFunction(newGovernorateOptions, newGovernorateInner, null);
 });
 
 /**** delete lecture* */
@@ -82,9 +134,9 @@ let delBtns = document.querySelectorAll(".delete-lec");
 let delPopupParagraph = document.querySelector(".del-lesson");
 
 delBtns.forEach((ele) => {
-	ele.addEventListener("click", function (e) {
-		delPopupParagraph.textContent = ele
-			.closest("tr")
-			.querySelector(".name-lesson").textContent;
-	});
+    ele.addEventListener("click", function (e) {
+        delPopupParagraph.textContent = ele
+            .closest("tr")
+            .querySelector(".name-lesson").textContent;
+    });
 });
