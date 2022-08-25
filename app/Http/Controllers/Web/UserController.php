b7d498d3b294b5d4b706852e3f38bba2da92c65e
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-25 16:51:52 +0200

    Edit Page

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -26,68 +26,76 @@ class UserController extends Controller
             return abort(404);
         }
     }
-    public function edit($id){
+    public function edit($id)
+    {
         $user = Auth::user();
         $student = User::find($id);
-        if ($user && $student && $user->role->number < 4):
-            return view('Admin.editStudent',['user'=>$user]);
-        else:
+        if ($user && $student && $user->role->number < 4) :
+            return view('Admin.editStudent', ['user' => $student]);
+        else :
             return abort(404);
         endif;
     }
     public function update(Request $request)
     {
         $user = Auth::user();
-        if ($user) :
-            $data = $request->all();
+        if (!$user) :
+            return redirect(route('login'));
+        endif;
+        if ($user->role->number >= 4) :
+            return abort(403);
+        endif;
 
-            Validator::make($data, [
-                'name' => ['required', 'string', 'max:255'],
-                'phoneNumber' => ['required', 'string', $data['phoneNumber'] != $user->phone_number ? 'unique:users,phone_number' : '', 'max:13', 'min:11'],
-                'parentPhoneNumber' => ['required', 'string', 'max:13', 'min:11'],
-                'grade' => ['required', 'string', 'exists:grades,name'],
-                'governorate' => ['required', 'string', 'exists:governorates,name'],
-                'center' => ['nullable', 'string', 'exists:centers,name'],
-            ], [
-                'name.required' => "الاسم مطلوب",
-                'name.string' => "الاسم يجب ان يكون حروف",
-                'name.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
+        $data = $request->all();
+        Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'phoneNumber' => ['required', 'string', $data['phoneNumber'] != $user->phone_number ? 'unique:users,phone_number' : '', 'max:13', 'min:11'],
+            'parentPhoneNumber' => ['required', 'string', 'max:13', 'min:11'],
+            'grade' => ['required', 'string', 'exists:grades,name'],
+            'governorate' => ['required', 'string', 'exists:governorates,name'],
+            'center' => ['nullable', 'string', 'exists:centers,name'],
+        ], [
+            'name.required' => "الاسم مطلوب",
+            'name.string' => "الاسم يجب ان يكون حروف",
+            'name.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
 
-                'email.required' => "البريد الالكتروني مطلوب",
-                'email.string' => "البريد الالكتروني يجب ان يكون حروف",
-                'email.email' => "البرجاء ادخال البريد الالكتروني بشكل صحيح",
-                'email.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
-                'email.unique' => "البريد الالكتروني موجود مسبقا",
+            'email.required' => "البريد الالكتروني مطلوب",
+            'email.string' => "البريد الالكتروني يجب ان يكون حروف",
+            'email.email' => "البرجاء ادخال البريد الالكتروني بشكل صحيح",
+            'email.max' => "أكبر عدد ممكن من الحروف هو 255 حرف",
+            'email.unique' => "البريد الالكتروني موجود مسبقا",
 
-                'password.required' => "كلمة المرور مطلوبة",
-                'password.string' => "يجب أن تكون كلمة المرور عبارة عن نص",
-                'password.min' => "أقل عدد ممكن من الحروف هو 8 حروف",
-                'password.confirmed' => "كلمة السر غير متطابقة",
+            'password.required' => "كلمة المرور مطلوبة",
+            'password.string' => "يجب أن تكون كلمة المرور عبارة عن نص",
+            'password.min' => "أقل عدد ممكن من الحروف هو 8 حروف",
+            'password.confirmed' => "كلمة السر غير متطابقة",
 
-                'grade.required' => "المرحلة التعليمية مطلوبة",
-                'grade.string' => "المرحلة التعليمية يجب ان يكون حروف",
-                'grade.exists' => "الرجاء اختيار مرحلة دراسية صحيحة",
+            'grade.required' => "المرحلة التعليمية مطلوبة",
+            'grade.string' => "المرحلة التعليمية يجب ان يكون حروف",
+            'grade.exists' => "الرجاء اختيار مرحلة دراسية صحيحة",
 
-                'phoneNumber.required' => "رقم الهاتف مطلوب",
-                'phoneNumber.unique' => "رقم الهاتف موجود بالفعل",
-                'phoneNumber.max' => "يجب أن لا يزيد عدد الأرقام عن 13 رقم",
-                'phoneNumber.min' => "يجب أن لا يقل عدد الأرقام عن 11 رقم",
-                'phoneNumber.starts_with' => "يجب أن يبدأ رقم الهاتف بـ0 أو +20",
+            'phoneNumber.required' => "رقم الهاتف مطلوب",
+            'phoneNumber.unique' => "رقم الهاتف موجود بالفعل",
+            'phoneNumber.max' => "يجب أن لا يزيد عدد الأرقام عن 13 رقم",
+            'phoneNumber.min' => "يجب أن لا يقل عدد الأرقام عن 11 رقم",
+            'phoneNumber.starts_with' => "يجب أن يبدأ رقم الهاتف بـ0 أو +20",
 
-                'parentPhoneNumber.required' => "رقم هاتف ولي الأمر مطلوب",
-                'parentPhoneNumber.max' => "يجب أن لا يزيد عدد الأرقام عن 13 رقم",
-                'parentPhoneNumber.min' => "يجب أن لا يقل عدد الأرقام عن 11 رقم",
-                'parentPhoneNumber.starts_with' => "يجب أن يبدأ رقم الهاتف بـ0",
+            'parentPhoneNumber.required' => "رقم هاتف ولي الأمر مطلوب",
+            'parentPhoneNumber.max' => "يجب أن لا يزيد عدد الأرقام عن 13 رقم",
+            'parentPhoneNumber.min' => "يجب أن لا يقل عدد الأرقام عن 11 رقم",
+            'parentPhoneNumber.starts_with' => "يجب أن يبدأ رقم الهاتف بـ0",
 
-                'governorate.required' => "المحافظة مطلوبة",
-                'governorate.string' => "المحافظة يجب ان يكون حروف",
-                'governorate.exists' => "الرجاء اختيار محافظة صحيحة",
+            'governorate.required' => "المحافظة مطلوبة",
+            'governorate.string' => "المحافظة يجب ان يكون حروف",
+            'governorate.exists' => "الرجاء اختيار محافظة صحيحة",
 
-                'center.string' => "المركز يجب ان يكون حروف",
-                'center.exists' => "الرجاء اختيار مركز صحيح",
-            ])->validate();
+            'center.string' => "المركز يجب ان يكون حروف",
+            'center.exists' => "الرجاء اختيار مركز صحيح",
+        ])->validate();
 
-            $user->update([
+        $student = User::Find($data['id']);
+        if ($student) :
+            $student->update([
                 'name' => $data['name'],
                 'phone_number' => $data['phoneNumber'],
                 'parent_phone_number' => $data['parentPhoneNumber'],
@@ -95,6 +103,8 @@ class UserController extends Controller
                 'governorate_id' => Governorate::where('name', $data['governorate'])->first()->id,
                 'center_id' => $data['center'] ? Center::where('name', $data['center'])->first()->id : 1,
             ]);
+        else:
+            return abort(404);
         endif;
         return redirect()->back();
     }
@@ -103,7 +113,7 @@ class UserController extends Controller
         $user = Auth::user();
         if ($user ? $user->role->number < 4 : false) :
             return view('Admin.addStudent');
-        esle:
+            esle:
             return abort(404);
         endif;
     }
