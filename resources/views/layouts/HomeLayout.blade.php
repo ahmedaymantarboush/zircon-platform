9a7e47d280f8c694cadb3bb408934cf9d6dd2c67
ali alaa <alialaa932003@gmail.com>
2022-08-26 00:57:57 +0200

    s

--- resources/views/layouts/HomeLayout.blade.php
+++ resources/views/layouts/HomeLayout.blade.php
@@ -248,7 +248,7 @@
     <!--font awesome-->
     <script src="{{ URL::asset('js/all.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js"></script>
 
     <!--jquery js-->
     <script src="{{ URL::asset('js/jquery.min.js') }}"></script>
