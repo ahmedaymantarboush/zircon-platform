e75140c42d48cb79c0d8a74371838e4b57c04e33
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-25 02:25:30 +0200

    add player files to layout

--- resources/views/layouts/lectureLayout.blade.php
+++ resources/views/layouts/lectureLayout.blade.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="{{asset('lectureAssets/css/lectureviewer.css')}}">
     @include('includes.appUrl')
+    <link rel="stylesheet" href="{{ asset('css/mediaPlayer.css') }}">
     <script src="{{asset('js/jquery.min.js')}}" ></script>
     {{--lecture name--}}
 
@@ -61,6 +62,8 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="{{asset('lectureAssets/js/lectureviewer.js')}}"></script>
+    <script src="{{ asset('js/mediaPlayer.js') }}"></script>
+
     {{-- Prevent Rigth click ,copy ,cut and paste --}}
 {{--    <script>--}}
 {{--        $(document).ready(function (){--}}
