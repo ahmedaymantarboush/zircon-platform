4aab77628292e972da36da2cd109df58c7730b61
ali alaa <alialaa932003@gmail.com>
2022-08-25 22:24:00 +0200

    s

---
+++ public/admin/assets/js/allCards.js
@@ -0,0 +1,45 @@
+/// run button
+openBtn.forEach((ele) => {
+    ele.addEventListener("click", function () {
+        trTable.forEach((e) => {
+            let myElement = ele.parentElement.parentElement;
+            if (e == ele.parentElement.parentElement) {
+                myElement.classList.toggle("active-tr");
+
+                myElement
+                    .querySelector(".open-tr svg")
+                    .classList.toggle("fa-minus");
+                myElement
+                    .querySelector(".open-tr svg")
+                    .classList.toggle("fa-plus");
+                return;
+            }
+
+            e.querySelector(".open-tr svg").classList.remove("fa-minus");
+            e.querySelector(".open-tr svg").classList.add("fa-plus");
+
+            e.classList.remove("active-tr");
+        });
+    });
+});
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+/**text editor */
+ClassicEditor.create(document.querySelector(".text-editor2"), {
+    language: {
+        // The UI will be English.
+        ui: "en",
+
+        // But the content will be edited in Arabic.
+        content: "ar",
+    },
+})
+    .then((editor) => {
+        const wordCountPlugin = editor.plugins.get("WordCount");
+        const wordCountWrapper = document.getElementById("word-count");
+
+        wordCountWrapper.appendChild(wordCountPlugin.wordCountContainer);
+        window.editor = editor;
+    })
+    .catch((err) => {});
