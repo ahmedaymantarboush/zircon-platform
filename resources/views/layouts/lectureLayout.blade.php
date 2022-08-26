6b6212e7f97eb2a3723671b91c4a306ce0c52b12
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-08-26 02:44:15 +0200

    lectureviewer.js Ajax updates

--- resources/views/layouts/lectureLayout.blade.php
+++ resources/views/layouts/lectureLayout.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('lectureAssets/css/lectureviewer.css') }}">
     @include('includes.appUrl')
     <link rel="stylesheet" href="{{ asset('css/mediaPlayer.css') }}">
