ee8f57498b4623ac8bd768f42c921094481a317a
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-08-26 06:12:13 +0200

    lectureviewer.js Ajax updates

--- public/lectureAssets/js/lectureviewer.js
+++ public/lectureAssets/js/lectureviewer.js
@@ -527,7 +527,8 @@ $(document).on('click','.takeExam',async function (){
             let addSelected ='';
             let addChecked = '';
             if(getQuestionVar.data.question.choices[j-1].id == getQuestionVar.data.question.choice){
-                addSelected ='selectedAnwser';
+                alert('تمم');
+                addSelected ='selectedAnser';
                 addChecked = 'checked';
             }
             examHTML += '<div class="col-12">\n' +
