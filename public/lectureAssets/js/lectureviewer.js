fb6a8a121b3f80f248e4fecece414a2183628a81
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-08-26 21:15:19 +0200

    lectureviewer.js Ajax updates

--- public/lectureAssets/js/lectureviewer.js
+++ public/lectureAssets/js/lectureviewer.js
@@ -510,7 +510,7 @@ $(document).on('click','.takeExam',async function (){
     examHTML += `<div class="swiper-slide">
                                 <button class="tab-item">
                                     <span class="tab-num" style="display: none"></span>
-                                    <span class="tab-num2" ><i class="fa-solid fa-calendar-days"></i></span>
+                                    <span class="tab-num2" ><i class="fa-solid fa-check-double"></i></span>
                                 </button>
                             </div>`;
     examHTML += '</div>\n' +
