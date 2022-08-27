1640baf05d2494213b2516fd6241a3dcc3c2a031
ali alaa <alialaa932003@gmail.com>
2022-08-27 21:46:31 +0200

    s

--- public/admin/assets/js/myLocations.js
+++ public/admin/assets/js/myLocations.js
@@ -164,6 +164,11 @@ document.querySelector("table").addEventListener("click", async function (e) {
     };
     let inputId = document.querySelector("#editLocationModal #trId");
     inputId.value = dataId;
+    let delCenter = document.querySelector(".del-lesson");
+    delCenter.innerHTML = e.target
+        .closest("tr")
+        .querySelector(".question-code")
+        .getAttribute("data-bs-original-title");
     document
         .querySelector(".delCenter")
         .addEventListener("submit", async function () {
@@ -176,10 +181,5 @@ document.querySelector("table").addEventListener("click", async function (e) {
                 e
             );
             console.log(myResponse);
-            let delCenter = document.querySelector(".del-lesson");
-            delCenter.innerHTML = e.target
-                .closest("tr")
-                .querySelector(".question-code")
-                .getAttribute("data-bs-original-title");
         });
 });
