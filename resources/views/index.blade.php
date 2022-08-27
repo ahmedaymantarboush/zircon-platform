7805be9368f31bf6eedcb3aac24f807a3c184d04
ali alaa <alialaa932003@gmail.com>
2022-08-27 05:07:06 +0200

    s

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -293,6 +293,15 @@
     </div>
     </div>
 </section>
+<div class="preloader">
+    <figure>
+        <div class="dot white"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+    </figure>
+</div>
 @endsection
 
 @section('javascript')
