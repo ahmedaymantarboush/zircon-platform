da4f9b5ae063325e481f362dfdd6922e261b388b
ali alaa <alialaa932003@gmail.com>
2022-08-27 05:35:20 +0200

    s

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -330,6 +330,11 @@
         <hr />
         <hr />
     </div>
+    <div class="atom">
+        <div class="electron"></div>
+        <div class="electron-alpha"></div>
+        <div class="electron-omega"></div>
+    </div>
 
 </div>
 @endsection
