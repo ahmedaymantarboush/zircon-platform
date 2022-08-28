260ff0006b053b8315eaeb64e76c567c687ae592
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-28 05:46:04 +0200

    gg

--- resources/views/Home/grades.blade.php
+++ resources/views/Home/grades.blade.php
@@ -1,65 +1,71 @@
 @extends('layouts.HomeLayout')
 @section('css')
-<!-- css file -->
-<link rel="stylesheet" href="{{ URL::asset('css/levels.css') }} " class="rel">
-<link rel="stylesheet" href="{{ URL::asset('css/levels-responsive.css') }} " class="rel">
+    <!-- css file -->
+    <link rel="stylesheet" href="{{ URL::asset('css/levels.css') }} " class="rel">
+    <link rel="stylesheet" href="{{ URL::asset('css/levels-responsive.css') }} " class="rel">
 @endsection
 @section('content')
-<section class="header grade {{$lectures[0]->grade->id}}">
-    @php
-    $grades = [
-    '1' => "الأول الثانوي",
-    '2' => "الثاني الثانوي",
-    '3' => "الثالث الثانوي",
-    ]
-    
-    @endphp
-    <h2 class='pageName'>شهور الصف <span>{{$grades[$lectures[0]->grade->id]}}</span></h2>
-</section>
-<section class='levelsBox'>
-    <div class="container">
-        <div class="row">
-            @foreach ($lectures as $lecture)
-            <div class="col-lg-12 col-md-6 gs_reveal ">
-                <a href='{{route('months.show',$lecture->slug)}}' class="levelItem grade{{$lecture->grade->id}}">
-                    <span class='typeGrade'>{{$lecture->grade->name}}</span>
-                    <div class="row">
-                        <div class="col-lg-3">
-                            <div class="levelImage">
-                                <img src="{{ $lecture->poster }}" alt="">
-                            </div>
-                        </div>
-                        <div class="col-lg-7">
-                            <div class="levelContent">
-                                <h3 class='levelName'>{{$lecture->title}}</h3>
-                                <p class='levelDescription'>{{$lecture->short_description}}</p>
-                                <div class="levelDetails">
-                                    <div class="detailItem">
-                                        <span class='detailIcon'><i class="fa-solid fa-photo-film"></i></span>
-                                        <span
-                                            class='detailContent'>{{ $lecture->time >= 3600 ? number_format($lecture->time / 3600, 2) . ' ساعة' : number_format($lecture->time / 60, 2) . ' دقيقة' }}
-                                            من الفيديو</span>
+    <section class="header grade {{ $lectures[0]->grade->id }}">
+        @php
+            $grades = [
+                '1' => 'الأول الثانوي',
+                '2' => 'الثاني الثانوي',
+                '3' => 'الثالث الثانوي',
+            ];
+            
+        @endphp
+        <h2 class='pageName'>شهور الصف <span>{{ $grades[$gradeId] }}</span></h2>
+    </section>
+    <section class='levelsBox'>
+        <div class="container">
+            <div class="row">
+                @if ($lectures)
+                    @foreach ($lectures as $lecture)
+                        <div class="col-lg-12 col-md-6 gs_reveal ">
+                            <a href='{{ route('months.show', $lecture->slug) }}'
+                                class="levelItem grade{{ $lecture->grade->id }}">
+                                <span class='typeGrade'>{{ $lecture->grade->name }}</span>
+                                <div class="row">
+                                    <div class="col-lg-3">
+                                        <div class="levelImage">
+                                            <img src="{{ $lecture->poster }}" alt="">
+                                        </div>
                                     </div>
-                                    <div class="detailItem">
-                                        <span class='detailIcon'><i class="fa-solid fa-clipboard-question"></i></span>
-                                        <span class='detailContent'>{{$lecture->total_questions_count}} من
-                                            الاسئلة</span>
+                                    <div class="col-lg-7">
+                                        <div class="levelContent">
+                                            <h3 class='levelName'>{{ $lecture->title }}</h3>
+                                            <p class='levelDescription'>{{ $lecture->short_description }}</p>
+                                            <div class="levelDetails">
+                                                <div class="detailItem">
+                                                    <span class='detailIcon'><i class="fa-solid fa-photo-film"></i></span>
+                                                    <span
+                                                        class='detailContent'>{{ $lecture->time >= 3600 ? number_format($lecture->time / 3600, 2) . ' ساعة' : number_format($lecture->time / 60, 2) . ' دقيقة' }}
+                                                        من الفيديو</span>
+                                                </div>
+                                                <div class="detailItem">
+                                                    <span class='detailIcon'><i
+                                                            class="fa-solid fa-clipboard-question"></i></span>
+                                                    <span class='detailContent'>{{ $lecture->total_questions_count }} من
+                                                        الاسئلة</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <div class="levelPrice">
+                                            <span class='iconPrice'><i class="fa-solid fa-tags"></i></span>
+                                            <span class='price'>{{ getPrice($lecture) }}</span>
+                                            <span class='priceUnit'>ج.م</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
-                        <div class="col-lg-2">
-                            <div class="levelPrice">
-                                <span class='iconPrice'><i class="fa-solid fa-tags"></i></span>
-                                <span class='price'>{{getPrice($lecture)}}</span>
-                                <span class='priceUnit'>ج.م</span>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            @endforeach
-            {{-- <div class="col-lg-12 col-md-6">
+                    @endforeach
+                @else
+                    <p>حط هنا أي ماسدج</p>
+                @endif
+                {{-- <div class="col-lg-12 col-md-6">
                     <a href='#' class="levelItem grade{{$lecture->grade->id}}">
             <span class='typeGrade'>الصف الاول الثانوي</span>
             <div class="row">
@@ -132,10 +138,10 @@
             </a>
         </div> --}}
 
-    </div>
-    </div>
-</section>
+            </div>
+        </div>
+    </section>
 @endsection
 @section('javascript')
-<script src="{{ URL::asset('js/levels.js') }}"></script>
+    <script src="{{ URL::asset('js/levels.js') }}"></script>
 @endsection
