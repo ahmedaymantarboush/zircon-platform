699cb10c15114802e443f18cb35fa78a2fa9db9a
ali alaa <alialaa932003@gmail.com>
2022-08-28 02:11:34 +0200

    s

--- resources/views/Admin/students.blade.php
+++ resources/views/Admin/students.blade.php
@@ -620,7 +620,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary">
+                    <button type="button" class="btn btn-primary printBtn">
                         طباعة
                     </button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
