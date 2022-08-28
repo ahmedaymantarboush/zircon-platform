8162b21bdb35a5f2d81ae5e931e57861f13e2f05
ali alaa <alialaa932003@gmail.com>
2022-08-28 03:48:38 +0200

    s

--- public/admin/assets/js/examsStudents.js
+++ public/admin/assets/js/examsStudents.js
@@ -31,5 +31,124 @@ $(function () {
 });
 let barcodeText = document.querySelector(".barcodeText");
 JsBarcode("#profileBarCode", barcodeText.textContent);
-let delBtns = document.querySelectorAll(".delete-lec");
-let delPopupParagraph = document.querySelector(".del-lesson");
+let editFun = async function (url, myData, el = null) {
+    try {
+        let postData = await fetch(url, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "X-CSRF-TOKEN": window.csrf_token.value,
+            },
+            body: myData,
+        });
+
+        let responseData = await postData.json();
+
+        if (postData.status == 200) {
+            return responseData;
+        }
+        if (postData.status == 404) {
+            return null;
+        }
+        return null;
+    } catch (err) {}
+};
+
+document.querySelector("table").addEventListener("click", async function (e) {
+    if (!e.target.classList.contains("handStudent")) return;
+    let dataId = e.target.closest("tr").dataset.id;
+
+    let sendObj = {
+        id: dataId,
+    };
+
+    form = new FormData();
+    form.append("data", JSON.stringify(sendObj));
+
+    let myResponse = await editFun(
+        `${window.location.protocol}//${window.location.host}/api/users/hanging`,
+        form,
+        e
+    );
+
+    let objData = myResponse.data;
+    if (objData != null) {
+        location.reload();
+    }
+});
+document.querySelector("table").addEventListener("click", async function (e) {
+    if (!e.target.classList.contains("studentCard")) return;
+    let dataId = e.target.closest("tr").dataset.id;
+
+    let sendObj = {
+        id: dataId,
+    };
+
+    form = new FormData();
+    form.append("data", JSON.stringify(sendObj));
+
+    let myResponse = await editFun(
+        `${window.location.protocol}//${window.location.host}/api/users/getStudentCardData`,
+        form,
+        e
+    );
+    let objData = myResponse.data;
+    console.log(objData);
+    let cardImage = document.querySelector("#studentCard .profileImg img");
+    let profileName = document.querySelector("#studentCard .profileName");
+    let profileType = document.querySelector("#studentCard .profileType");
+    let cardName = document.querySelector("#studentCard .cardName");
+    let cardNumber = document.querySelector("#studentCard .cardNumber");
+    let cardGrade = document.querySelector("#studentCard .cardGrade");
+    let cardParentNumber = document.querySelector(
+        "#studentCard .cardParentNumber"
+    );
+    let cardGovernorate = document.querySelector(
+        "#studentCard .cardGovernorate"
+    );
+    let barcodeText = document.querySelector("#studentCard .barcodeText");
+    cardImage.setAttribute("src", objData.image);
+    profileName.innerHTML = objData.name;
+    profileType.innerHTML = objData.role;
+    cardName.innerHTML = objData.name;
+    cardNumber.innerHTML = objData.phoneNumber;
+    cardParentNumber.innerHTML = objData.parentPhoneNumber;
+    cardGrade.innerHTML = objData.grade;
+    cardGovernorate.innerHTML = objData.governorate;
+    barcodeText.innerHTML = objData.code;
+    JsBarcode("#profileBarCode", barcodeText.innerHTML);
+});
+
+document.querySelector("table").addEventListener("click", function (e) {
+    if (!e.target.classList.contains("delete-lec")) return;
+    let testi = document.querySelector(".del-lesson");
+    testi.innerHTML = e.target
+        .closest("tr")
+        .querySelector(".question-code")
+        .getAttribute("data-bs-original-title");
+    let inputId = document.querySelector('#deleteStudent input[name="id"]');
+    inputId.value = e.target.closest("tr").dataset.id;
+});
+printPage = document.querySelector("#printPage");
+printPage.style.display = "none";
+
+function PrintElement(selector, customStyles = "") {
+    Popup(document.querySelector(selector).outerHTML, customStyles);
+}
+
+function Popup(data, customStyles = "") {
+    const printPageWindow = printPage.contentWindow;
+
+    const printPageDocument = printPageWindow.document;
+    printPageDocument.querySelector(
+        "body"
+    ).innerHTML = `<style>${customStyles}</style>${data}`;
+    printPageWindow.focus();
+    printPageWindow.print();
+    printPageWindow.close();
+}
+
+let printBtn = document.querySelector(".printBtn");
+printBtn.addEventListener("click", function () {
+    PrintElement(".profileDetails");
+});
--- resources/views/Admin/examStudents.blade.php
+++ resources/views/Admin/examStudents.blade.php
@@ -416,7 +416,7 @@
                         <div class="profileContent">
                             <div class="personalInformation">
                                 <h3 class="profileName">
-                                    عبدالرحمن مصطفى محمود
+                                    ....
                                 </h3>
                                 <span class="profileType">طالب</span>
                             </div>
@@ -431,23 +431,23 @@
                                 </div>
                                 <div class="item">
                                     <span class="type">الاسم:</span>
-                                    <span class="info">عبدالرحمن مصطفى محمود</span>
+                                    <span class="info cardName">....</span>
                                 </div>
                                 <div class="item">
                                     <span class="type">المرحلة الدراسية:</span>
-                                    <span class="info">الصف الثالث الثانوي</span>
+                                    <span class="info cardGrade">....</span>
                                 </div>
                                 <div class="item">
                                     <span class="type">الرقم:</span>
-                                    <span class="info">01234567891</span>
+                                    <span class="info cardNumber">....</span>
                                 </div>
                                 <div class="item">
                                     <span class="type">رقم ولي الامر:</span>
-                                    <span class="info">01234567891</span>
+                                    <span class="info cardParentNumber">....</span>
                                 </div>
                                 <div class="item">
                                     <span class="type"> المحافظة:</span>
-                                    <span class="info">القاهرة</span>
+                                    <span class="info cardGovernorate">....</span>
                                 </div>
                                 <div class="item">
                                     <span class="type">مكان الحضور:</span>
@@ -455,14 +455,14 @@
                                 </div>
                                 <div class="barcodeBox">
                                     <svg class="barcode" id="profileBarCode"></svg>
-                                    <span class="barcodeText">as4df5ef12as4df5ef1212345</span>
+                                    <span class="barcodeText">....</span>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">
+                    <button type="button" class="btn btn-primary printBtn">
                         طباعة
                     </button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
