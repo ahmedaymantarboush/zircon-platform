e80af92638881059a18ad39f05814ae88349be67
Eng-Abdelrahman-Mostafa <abdelrahmanmostafa785@gmail.com>
2022-08-28 03:01:38 +0200

    lectureviewer.js Ajax updates 2

--- public/admin/assets/js/add-exam.js
+++ public/admin/assets/js/add-exam.js
@@ -482,7 +482,6 @@ $(document).on('change','select.dynamicQuestion',async function (){
     let partSelect = $(this).val();
     let question_box = $(this).parent().closest(".question-box");
     let countValue = $(question_box).find(".countInput").val();
-    alert($(".countInput").val());
     if(!$(this).parent().closest(".question-box").attr('data-id')){
         //Ajax
         form3 = new FormData()
