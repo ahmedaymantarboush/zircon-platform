a83537988b9494edcd76d192dca133aa03e3fb79
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-30 00:20:19 +0200

    g

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,10 @@
 @endsection
 @section('css')
 {{-- <!--swiper--> --}}
-<link rel="stylesheet" href="{{ URL::asset('css/swiper.bundle.min.css') }}">
+{{-- <link rel="stylesheet" href="{{ URL::asset('css/swiper.bundle.min.css') }}"> --}}
 {{-- <!-- css file --> --}}
 <link rel="stylesheet" href="{{ URL::asset('css/home.css') }} " class="rel">
 <link rel="stylesheet" href="{{ URL::asset('css/home-responsive.css') }} " class="rel">
-<script></script>
 @endsection
 @section('content')
 <header class="">
