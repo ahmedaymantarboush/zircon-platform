9ccbcf44c82bceb1af6df45d2cd8c314ed407759
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-08-30 09:25:59 +0200

    g

--- resources/views/Admin/editExam.blade.php
+++ resources/views/Admin/editExam.blade.php
@@ -39,7 +39,7 @@
                     <span>تعديل الامتحان</span>
                 </div>
                 <div class="main_exam_info">
-                    @include('components.Admin.examForm', ['exam' => $exam])
+                    @include('components.admin.examForm', ['exam' => $exam])
                 </div>
                 <div class="add_questions" style="margin-bottom: 20px">
                     <div class="row">
