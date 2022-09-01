885523220cdc101ec2e62050336968996fe94d48
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 16:05:34 +0200

    s

--- resources/views/layouts/HomeLayout.blade.php
+++ resources/views/layouts/HomeLayout.blade.php
@@ -15,7 +15,8 @@
         href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600&display=swap"
         rel="stylesheet" />
 
-    <link rel="stylesheet" href="{{ URL::asset('css/all.min.css') }}">
+    <!-- <link rel="stylesheet" href="{{ URL::asset('css/all.min.css') }}"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-rtl.min.css') }}">
     @include('includes.appUrl')
