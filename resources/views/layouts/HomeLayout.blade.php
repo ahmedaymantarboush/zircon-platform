2cc214aa1dc6d505642811aeb210c9ef8bcf282d
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 16:28:24 +0200

    s

--- resources/views/layouts/HomeLayout.blade.php
+++ resources/views/layouts/HomeLayout.blade.php
@@ -16,7 +16,7 @@
         rel="stylesheet" />
 
     <!-- <link rel="stylesheet" href="{{ URL::asset('css/all.min.css') }}"> -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-rtl.min.css') }}">
     @include('includes.appUrl')
