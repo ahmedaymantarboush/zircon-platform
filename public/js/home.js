54b9f18beca3efce835a2de7be616d165b3c7389
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 17:58:58 +0200

    s

--- public/js/home.js
+++ public/js/home.js
@@ -22,44 +22,60 @@ function animateFrom(elem, direction) {
     direction = direction || 1;
     var x = 0,
         y = direction * 100;
-    if(elem.classList.contains("gs_reveal_fromLeft")) {
-      x = -100;
-      y = 0;
+    if (elem.classList.contains("gs_reveal_fromLeft")) {
+        x = -100;
+        y = 0;
     } else if (elem.classList.contains("gs_reveal_fromRight")) {
-      x = 100;
-      y = 0;
+        x = 100;
+        y = 0;
+    } else if (elem.classList.contains("gs_reveal_fromDown")) {
+        x = 0;
+        y = -100;
+    } else if (elem.classList.contains("gs_reveal_fromUp")) {
+        x = 0;
+        y = 100;
     }
     elem.style.transform = "translate(" + x + "px, " + y + "px)";
     elem.style.opacity = "0";
-    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-      duration: 1.25, 
-      x: 0,
-      y: 0, 
-      autoAlpha: 1, 
-      ease: "expo", 
-      overwrite: "auto"
-    });
-  }
-  
-  function hide(elem) {
-    gsap.set(elem, {autoAlpha: 0});
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
+    gsap.fromTo(
+        elem,
+        { x: x, y: y, autoAlpha: 0 },
+        {
+            duration: 2,
+            x: 0,
+            y: 0,
+            autoAlpha: 1,
+            ease: "expo",
+            overwrite: "auto",
+        }
+    );
+}
+
+function hide(elem) {
+    gsap.set(elem, { autoAlpha: 0 });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     gsap.registerPlugin(ScrollTrigger);
-    
-    gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
-      hide(elem); // assure that the element is hidden when scrolled into view
-      
-      ScrollTrigger.create({
-        trigger: elem,
-        onEnter: function() { animateFrom(elem) }, 
-        onEnterBack: function() { animateFrom(elem, -1) },
-        onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-      });
+
+    gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+        hide(elem); // assure that the element is hidden when scrolled into view
+
+        ScrollTrigger.create({
+            trigger: elem,
+            onEnter: function () {
+                animateFrom(elem);
+            },
+            onEnterBack: function () {
+                animateFrom(elem, -1);
+            },
+            onLeave: function () {
+                hide(elem);
+            }, // assure that the element is hidden when scrolled into view
+        });
     });
-  });
-  
+});
+
 /////////////////////////////////////
 ///// start navbar /////////////////
 ///////////////////////////////////
@@ -138,8 +154,10 @@ window.addEventListener("scroll", navProgFunction);
 /////////////////////////////////////
 ///// start header /////////////////
 ///////////////////////////////////
-
-AOS.init();
+$(document).ready(function(){
+    $('.animate').scrolla();
+});
+  AOS.init();
 
 // [01-Atom]
 $(document).ready(function () {
