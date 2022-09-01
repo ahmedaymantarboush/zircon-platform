607ba686af9cd3e43c49a2541525dae3dedd6e9e
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 20:44:28 +0200

    s

--- public/js/home.js
+++ public/js/home.js
@@ -18,7 +18,7 @@
 // // Add all html elements with attribute data-triggerAlways, these elements will always be triggered
 // trigger.add('[data-triggerAlways]', { once: false })
 $(".foo, .bar").smoove({
-    offset  : '0',
+    offset  : '1%',
     // moveX is overridden to -200px for ".bar" object
     moveX   : '0',
     moveY   : '100px',
