a19c5695c60639c03164b9afe955468918a84169
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 18:09:26 +0200

    s

--- public/js/home.js
+++ public/js/home.js
@@ -55,26 +55,26 @@ function hide(elem) {
     gsap.set(elem, { autoAlpha: 0 });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    gsap.registerPlugin(ScrollTrigger);
+// document.addEventListener("DOMContentLoaded", function () {
+//     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
-        hide(elem); // assure that the element is hidden when scrolled into view
+//     gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+//         hide(elem); // assure that the element is hidden when scrolled into view
 
-        ScrollTrigger.create({
-            trigger: elem,
-            onEnter: function () {
-                animateFrom(elem);
-            },
-            onEnterBack: function () {
-                animateFrom(elem, -1);
-            },
-            onLeave: function () {
-                hide(elem);
-            }, // assure that the element is hidden when scrolled into view
-        });
-    });
-});
+//         ScrollTrigger.create({
+//             trigger: elem,
+//             onEnter: function () {
+//                 animateFrom(elem);
+//             },
+//             onEnterBack: function () {
+//                 animateFrom(elem, -1);
+//             },
+//             onLeave: function () {
+//                 hide(elem);
+//             }, // assure that the element is hidden when scrolled into view
+//         });
+//     });
+// });
 
 /////////////////////////////////////
 ///// start navbar /////////////////
