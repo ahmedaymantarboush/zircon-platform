d15a307d314da8cdeb77d251e475186e92235872
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-01 21:16:34 +0200

    s

--- public/js/home.js
+++ public/js/home.js
@@ -18,6 +18,7 @@
 // // Add all html elements with attribute data-triggerAlways, these elements will always be triggered
 // trigger.add('[data-triggerAlways]', { once: false })
 
+sal();
 
 // Now in your CSS add the following classes, this fades the [data-trigger] elements in and out
 
