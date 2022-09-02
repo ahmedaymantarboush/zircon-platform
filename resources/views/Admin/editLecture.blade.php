8e00765c36c76f25bf412d3fe2b4b3066739580a
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-02 03:49:30 +0200

    s

--- resources/views/Admin/editLecture.blade.php
+++ resources/views/Admin/editLecture.blade.php
@@ -623,6 +623,8 @@
 
 
 @section('javascript')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     {{-- <!-- bootstrap 4  --> --}}
     {{-- <!-- موجود هنا والايديت  --> --}}
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
