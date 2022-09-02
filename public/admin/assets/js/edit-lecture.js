6ea2262e6ad2d7ac22b6db4532f87cd1f2ead6b9
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-02 06:00:41 +0200

    s

--- public/admin/assets/js/edit-lecture.js
+++ public/admin/assets/js/edit-lecture.js
@@ -158,7 +158,7 @@ document
                 .addEventListener("submit", async function (event) {
                     event.preventDefault();
                     let saveObjSend = {
-                        id: lessAddress.value,
+                        title: lessAddress.value.trim(),
                     };
 
                     form = new FormData();
