84c00af1b79288f72e610f8c74a81bab5734cd1e
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-03 19:06:26 +0200

    s

--- resources/views/Admin/addExam.blade.php
+++ resources/views/Admin/addExam.blade.php
@@ -56,6 +56,7 @@
         }
     </style>
     {{-- <!-- Latest compiled and minified JavaScript --> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
     {{-- <!-- bootstrap 5  --> --}}
     {{-- <!-- موجود في كل الصفحات  --> --}}
