a47de123eac1296e9971028a821b60724df947cf
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-09-03 19:50:54 +0200

    g

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -373,7 +373,7 @@
         $date = date('Y-m-d', strtotime($balanceCard->used_at));
         if (!in_array($date,explode(',', $compareCurveDates))):
             $compareCurveDates .= "'" . $date . "',";
-            print($balanceCards->where('used_at','LIKE',"%$date%")->pluck('value'));
+            print($balanceCards);
             $compareCurveValues .= $balanceCards->where('used_at','LIKE',"%$date%")->pluck('value')->sum() . ',';
         endif;
     endforeach;
