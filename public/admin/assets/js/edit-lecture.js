404f567bcd63965345d79f134ee1bf7ec9e3ac90
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-03 19:55:36 +0200

    s

--- public/admin/assets/js/edit-lecture.js
+++ public/admin/assets/js/edit-lecture.js
@@ -205,4 +205,13 @@ document
             .querySelector(".type-less-name")
             .innerHTML.trim();
         trId.value = e.target.closest("section-lesson-item").dataset.item;
+
+        console.log(delLesson);
+        console.log(trId);
+        console.log(
+            e.target
+                .closest("section-lesson-item")
+                .querySelector(".type-less-name")
+                .innerHTML.trim()
+        );
     });
