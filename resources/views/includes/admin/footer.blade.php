2637bda6f038ec3b58855e9c8b3f6b0a9e62498a
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-05 00:36:18 +0200

    s

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -1,5 +1,8 @@
-{{-- <!--jquery-->
-<!-- موجود في كل الصفحات  --> --}}
+<footer>
+    <p class='copyRight'>
+         made with <i class="fa-solid fa-heart"></i> by <a href="#" class='zirconLink'>Zircon Tech 2022</a>
+    </p>
+</footer>
 @yield('javascript')
 
 </body>
