fc1d307f807b2e859694612c612f2d673b3f8a9f
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-05 00:39:50 +0200

    s

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <p class='copyRight'>
-         made with <i class="fa-solid fa-heart"></i> by <a href="#" class='zirconLink'>Zircon Tech 2022</a>
+         made with <span class='heartIcon'><i class="fa-solid fa-heart"></i></span> by <a href="#" class='zirconLink'>Zircon Tech</a> 2022
     </p>
 </footer>
 @yield('javascript')
