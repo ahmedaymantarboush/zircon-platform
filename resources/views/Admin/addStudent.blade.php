36a89c750d06c3e32b383e239ea4c5fc4f96cef6
Ali_Alaa_Elidn <66901446+alialaa932003@users.noreply.github.com>
2022-09-08 17:11:58 +0200

    s

--- resources/views/Admin/addStudent.blade.php
+++ resources/views/Admin/addStudent.blade.php
@@ -237,6 +237,9 @@
 
 
 @section('javascript')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
+    integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
     </script>
