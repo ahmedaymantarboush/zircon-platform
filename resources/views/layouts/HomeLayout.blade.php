339a5a98b6607509959eedc5075d361a5228257e
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2022-09-09 18:58:46 +0200

    commit

--- resources/views/layouts/HomeLayout.blade.php
+++ resources/views/layouts/HomeLayout.blade.php
@@ -264,27 +264,27 @@
             // $('body').bind('cut copy paste', function(e) {
             //     e.preventDefault();
             // })
-        //     $("body").on("contextmenu", function(e) {
-        //         return false;
-        //     })
-        //     document.onkeydown = function(e) {
-        //         if (event.keyCode == 123) {
-        //             return false;
-        //         }
-        //         if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        //             return false;
-        //         }
-        //         if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        //             return false;
-        //         }
-        //         if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        //             return false;
-        //         }
-        //         if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        //             return false;
-        //         }
-        //     }
-        // })
+            $("body").on("contextmenu", function(e) {
+                return false;
+            })
+            document.onkeydown = function(e) {
+                if (event.keyCode == 123) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+                    return false;
+                }
+                if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+                    return false;
+                }
+            }
+        })
     </script>
 </body>
 
